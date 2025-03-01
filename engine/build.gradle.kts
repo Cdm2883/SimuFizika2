5db864788e0fe7e3c5206df4094fc98894960c06
Cdm2883 <Cdm2883@hotmail.com>
2025-03-01 20:23:33 +0800

    ci: auto deploy site on gh pages

--- engine/build.gradle.kts
+++ engine/build.gradle.kts
@@ -11,3 +11,13 @@ kotlin {
         binaries.library()
     }
 }
+
+fun buildDir(path: String) = layout.buildDirectory.file(path).get().asFile
+infix fun String.copyTo(dest: String) = buildDir(this)
+    .copyRecursively(buildDir(dest), overwrite = true)
+tasks.named("jsBrowserDevelopmentLibraryDistribution") {
+    doLast { "dist/js/developmentLibrary" copyTo "dist/js/library" }
+}
+tasks.named("jsBrowserProductionLibraryDistribution") {
+    doLast { "dist/js/productionLibrary" copyTo "dist/js/library" }
+}
